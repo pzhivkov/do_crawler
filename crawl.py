1baf78829ea31e62035c2bbd3d690b4e1bf787c3
Peter Zhivkov <pzhivkov@gmail.com>
2016-06-03 15:05:57 -0400

    Add rudimentary graph output.

--- crawl.py
+++ crawl.py
@@ -2,24 +2,39 @@
 
 import logging
 from do_crawler.crawler import Crawler
+from do_crawler.sitemap_viz import print_sitemap
 
 
 def configure_logging():
+    """Configure logging to console at level INFO."""
+
     logger = logging.getLogger('do_crawler')
-    logger.setLevel(logging.INFO)
+
     console = logging.StreamHandler()
-    console.setFormatter(logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s'))
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+
     logger.addHandler(console)
+    logger.setLevel(logging.INFO)
+
+
+# --- Main function:
 
 
 def main():
     configure_logging()
 
+    # Start a parallel crawl.
     c = Crawler('http://digitalocean.com')
     try:
         c.parallel_crawl()
     except (KeyboardInterrupt, SystemExit) as _:
         c.pool.close()
+
+    # If we're done (or were interrupted), then output the result so far.
+    c.pool.join()
+    print_sitemap(c.sitemap)
+    if c.links_to_visit:
         print(c.links_to_visit)
 
 
---
+++ do_crawler/sitemap_viz.py
@@ -0,0 +1,35 @@
+from do_crawler.sitemap import (
+    Page,
+    SiteMap
+)
+from pprint import pformat
+from sys import stdout
+from textwrap import indent
+
+
+def print_page(page: Page, file):
+    """Output a page record."""
+    print("Page = " + str(page.urls), file=file)
+
+    print(indent("Links = " + pformat(page.links), '    '), file=file)
+    print(indent("Static Assets = " + pformat(page.static_assets), '    '), file=file)
+
+    print(file=file)
+
+
+def print_sitemap(sitemap: SiteMap, file=stdout):
+    """Generate a console dump of a site map."""
+
+    for page in sitemap.pages.values():
+        print_page(page, file)
+
+
+# --- Main function:
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
